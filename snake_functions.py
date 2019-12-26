faf4677d3bf0b47601ddeb288902fffd1c8f18e6
GoeroenM <goeroen.maaruf@gmail.com>
2019-12-26 14:39:45 +0100

    Made create_cube_from_block_data function.

--- snake_functions.py
+++ snake_functions.py
@@ -7,16 +7,18 @@ import copy
 # 2 columns named 'block' and 'location' where location is an np array with the
 # location in x, y, z coordinates.
 def initialize_cube(init_cube, snake, cube_size = 4):
-    cube = np.zeros((cube_size, cube_size, cube_size))
-    for i in init_cube.block:
-        location = init_cube[init_cube.block == i].location[(i - 1)]
-        if cube[location[0]][location[1]][location[2]] != 0:
-            print("You tried to put two blocks in the same location.\n")
-            print("Aborting cube initilisation, please fill in correct locations.")
-            cube = np.zeros((cube_size, cube_size, cube_size))
-            break
-        else:
-            cube[location[0]][location[1]][location[2]] = int(i)
+#    cube = np.zeros((cube_size, cube_size, cube_size))
+#    for i in init_cube.block:
+#        location = init_cube[init_cube.block == i].location[(i - 1)]
+#        if cube[location[0]][location[1]][location[2]] != 0:
+#            print("You tried to put two blocks in the same location.\n")
+#            print("Aborting cube initilisation, please fill in correct locations.")
+#            cube = np.zeros((cube_size, cube_size, cube_size))
+#            break
+#        else:
+#            cube[location[0]][location[1]][location[2]] = int(i)
+    
+    cube = create_cube_from_block_data(init_cube[['block', 'location']])
     
     # Now initialize the block data
     max_block = get_max_block(cube)
@@ -43,6 +45,21 @@ def initialize_cube(init_cube, snake, cube_size = 4):
     
     return(block_data, cube)
 
+# Function that creates a cube from the block data. This function removes the need
+# to always give cube as a function argument, as we can quickly construct it if
+# needed.
+def create_cube_from_block_data(block_data, cube_size = 4):
+    cube = np.zeros((cube_size, cube_size, cube_size))
+    for i in block_data.block:
+        location = block_data[block_data.block == i].location[(i - 1)]
+        if cube[location[0]][location[1]][location[2]] != 0:
+            print("You tried to put two blocks in the same location.\n")
+            print("Aborting cube initilisation, please fill in correct locations.")
+            cube = np.zeros((cube_size, cube_size, cube_size))
+            break
+        else:
+            cube[location[0]][location[1]][location[2]] = int(i)
+    return(cube)
 # Functions that updates a cube in case the block_data is more complete
 def update_cube_from_data(cube, block_data, verbose = False):
     max_block_cube = get_max_block(cube)
